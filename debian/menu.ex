b4a20bfa42270bc3352856d82be5a0a6eed891a9
mollef <franck.molle@ac-rouen.fr>
2018-03-10 02:09:15 +0100

    files package

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(sambaedu-config):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="sambaedu-config" command="/usr/bin/sambaedu-config"
---
+++ debian/sambaedu-config.default.ex
@@ -0,0 +1,10 @@
+# Defaults for sambaedu-config initscript
+# sourced by /etc/init.d/sambaedu-config
+# installed at /etc/default/sambaedu-config by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
