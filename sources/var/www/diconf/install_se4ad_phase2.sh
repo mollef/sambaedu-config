73d6ac8cb365842b6218794f7206a0801fdf34b2
GrosQuicK <ericmer@free.fr>
2018-09-09 11:24:32 +0200

    sed netbios

--- sources/var/www/diconf/install_se4ad_phase2.sh
+++ sources/var/www/diconf/install_se4ad_phase2.sh
@@ -557,7 +557,6 @@ if [ -e "$dir_export/smb.conf" ]; then
 	echo "Lancement de la migration du domaine NT4 vers Samba AD avec sambatool" 
 	go_on
 	echo -e "$COLCMD"
-	# sed "s/$netbios_name/se4ad/I" -i $dir_export/smb.conf
 	sed "s/netbios name = $netbios_name/netbios name = se4ad/I" -i $dir_export/smb.conf
 	sed "s/workgroup = $sambadomaine_old/workgroup = $sambadomaine_new/I" -i $dir_export/smb.conf
 	sed "s#passdb backend.*#passdb backend = ldapsam:ldap://$se4ad_ip#" -i $dir_export/smb.conf  
