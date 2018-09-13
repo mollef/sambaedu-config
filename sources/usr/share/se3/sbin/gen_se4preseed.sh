3ce8596a6405f83cb1e3c3b97ce76769b807b61b
GrosQuicK <ericmer@free.fr>
2018-09-13 08:54:33 +0200

    Add duplicate ldap test

--- sources/usr/share/se3/sbin/gen_se4preseed.sh
+++ sources/usr/share/se3/sbin/gen_se4preseed.sh
@@ -871,12 +871,16 @@ if [ -f $test_duplicate_sid ];then
 	echo "Test de la présence d'éventuels doublons dans l'annuaire"
 	duplicate_sid="$( python $test_duplicate_sid )"
 	if [ "$duplicate_sid"  != "" ];then
-		echo $duplicate_sid
+		$dialog_box  --backtitle "$BACKTITLE" --title "Doublons dans l'annuaire !" --msgbox "$duplicate_sid 
+		
+		Corriger cela dans l'interface : 
+			- Informations système => correction de problèmes => recherche des doublons ldap
+			- Gestion des parcs => Rechercher => doublons MAC"
 		erreur "Doublons dans l'annuaire, corriger cela dans l'interface : 
 - Informations système => correction de problèmes => recherche des doublons ldap
 - Gestion des parcs => Rechercher => doublons MAC"
 	else
-		echo "Pas de doublon détecté"
+		$dialog_box  --backtitle "$BACKTITLE" --title "Doublons dans l'annuaire !" --msgbox "Pas de doublon dans l'annuaire"
 	fi
 fi
 }
